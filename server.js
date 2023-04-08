943ace96f4ade3135bd3a021c347b85a557781b7
Hari Ganesan <hariganation@gmail.com>
2023-04-08 12:37:11 +0530

    updated base server

--- server.js
+++ server.js
@@ -4,10 +4,24 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const port = 3000;
 
-app.use(cors());
 app.use(bodyParser.json());
+app.use(cors());
 
-app.get('/api', (req, res) => {
-    res.send('Hello World!');
-    }
-);
+// Import routes
+const authRoutes = require('./microservices/authentication/routes');
+const catalogRoutes = require('./microservices/catalog/routes');
+const cartRoutes = require('./microservices/cart/routes');
+const orderRoutes = require('./microservices/order/routes');
+const paymentRoutes = require('./microservices/payment/routes');
+const userRoutes = require('./microservices/user/routes');
+
+app.use('/api/authentication', authRoutes);
+app.use('/api/catalog', catalogRoutes);
+app.use('/api/cart', cartRoutes);
+app.use('/api/order', orderRoutes);
+app.use('/api/payment', paymentRoutes);
+app.use('/api/user', userRoutes);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
