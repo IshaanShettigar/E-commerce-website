0a9af681cbb51c9edb310bdb407196014c1f4c32
Ishaan Shettigar <ishaanshettigar@gmail.com>
2023-04-13 09:54:04 +0530

    test not working

--- backend/auth/index.js
+++ backend/auth/index.js
@@ -1,14 +1,12 @@
 const express = require("express")
 const app = express()
 const bodyParser = require('body-parser');
-const { PORT } = require("./config")
-const authRouter = require("./auth/auth.js");
-// const cartRouter = require("./cart/cart");
+const { PORT } = require("../config")
+const authRouter = require("./auth");
 
 app.use(bodyParser.json());
 
 app.use("/auth", authRouter);
-// app.use("/cart", cartRouter);
 
 app.listen(PORT, () => {
     console.log(`Listening on ${PORT}`)
